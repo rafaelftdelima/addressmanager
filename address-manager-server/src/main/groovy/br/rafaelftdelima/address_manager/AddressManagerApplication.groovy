f825a289ec237741cd913f63cc429579adecced7
rafaelftdelima <rafaelftdelima@gmail.com>
2025-07-27 19:30:47 -0300

    Init project: backend structure

---
+++ address-manager-server/src/main/groovy/br/rafaelftdelima/address_manager/AddressManagerApplication.groovy
@@ -0,0 +1,13 @@
+package br.rafaelftdelima.address_manager
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AddressManagerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(AddressManagerApplication, args)
+	}
+
+}
